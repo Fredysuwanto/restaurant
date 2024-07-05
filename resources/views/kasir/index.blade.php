f80a31e8b6f6c6360967362016324a42ae0c7e3d
Fredysuwanto <fredysuwanto56@gmail.com>
2024-07-05 11:45:59 +0700

    ppush

--- resources/views/kasir/index.blade.php
+++ resources/views/kasir/index.blade.php
@@ -56,7 +56,7 @@ Kasir
     </table>
   </div>
 </div>
-
+<h1>tes</h1>
 <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 @if (session('success'))
